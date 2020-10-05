4a753205da5a987c1089ffcae86063233f15e996
justin-wittenmeier <71291781+justin-wittenmeier@users.noreply.github.com>
2020-10-05 14:08:36 -0500

    Create 8-ball.py

---
+++ 8-ball.py
@@ -0,0 +1,11 @@
+import random
+
+def shake():
+    r = ['Try again later.','Positive.','Negative.','The odds are good.','The odds are bad.','The future is hard to see right now try again later.','It is certain.','We can not know.','Never.','Maybe.','Yes.','No.']
+    print(random.choice(r))
+
+print('\n')
+
+shake()
+
+print('\n')
