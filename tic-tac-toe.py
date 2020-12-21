52b54d8fa846a262512145ba5e709897b6f37b7d
justin-wittenmeier <71291781+justin-wittenmeier@users.noreply.github.com>
2020-12-21 09:43:20 -0600

    Update tic-tac-toe.py
    Update win check to be in a loop instead of each individual outcome.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,85 +1,56 @@
 #Tic Tak Toe Update
 class Game(object):
     def __init__(self):
-        #generate board
+        #Empty board for game
         self.board = [' '] * 9
         
-        #turn tracker
+        #Sets current turn to player by default.
         self.current_turn = 'O'
 
-        #on/off switch
+        #On/off switch to restart the game
         self.game_on = False
 
-    #cleans the board
+    #Clear the board for a new game.
     def clearBoard(self):
         self.board = [' '] * 9
     
-    #prints the current board
+    #Print the board for the current match
     def printBoard(self): print(f'Current Board:\n{self.board[6]}|{self.board[7]}|{self.board[8]}\n-+-+-\n{self.board[3]}|{self.board[4]}|{self.board[5]}\n-+-+-\n{self.board[0]}|{self.board[1]}|{self.board[2]}')
     
-    #prints info version of board
+    #Let the player know how to make moves in the game.
     def printInfoBoard(self): print('Select Move:\n7|8|9\n-+-+-\n4|5|6\n-+-+-\n1|2|3')
 
-    #start game
+    #Idle Start Screen
     def startGame(self):
         input('Enter anything to start game: ')
         self.clearBoard()
         return True
 
-    #checks turn
+    #Cycle player/bot moves.
     def runGame(self):
         if self.current_turn == 'O': self.playerMove()
-        elif self.current_turn == 'X': self.botMove()
+        else: self.botMove()
 
-    #checks if game is over
+    #Check if the game is still in progress or not and gets the winner.
     def gameOver(self):
-        if self.board[0] == self.board[1] == self.board[2] != ' ':
-            self.printBoard()
-            print(f'\nWINNER: {self.board[0]}')
-            self.current_turn = self.board[0]
-            return False
-        elif self.board[3] == self.board[4] == self.board[5] != ' ':
-            self.printBoard()
-            print(f'\nWINNER: {self.board[3]}')
-            self.current_turn = self.board[3]
-            return False
-        elif self.board[6] == self.board[7] == self.board[8] != ' ':
-            self.printBoard()
-            print(f'\nWINNER: {self.board[6]}')
-            self.current_turn = self.board[6]
-            return False
-        elif self.board[2] == self.board[4] == self.board[6] != ' ':
-            self.printBoard()
-            print(f'\nWINNER: {self.board[6]}')
-            self.current_turn = self.board[6]
-            return False
-        elif self.board[0] == self.board[4] == self.board[8] != ' ':
-            self.printBoard()
-            print(f'\nWINNER: {self.board[0]}')
-            self.current_turn = self.board[0]
-            return False
-        elif self.board[0] == self.board[3] == self.board[6] != ' ':
-            self.printBoard()
-            print(f'\nWINNER: {self.board[0]}')
-            self.current_turn = self.board[0]
-            return False
-        elif self.board[1] == self.board[4] == self.board[7] != ' ':
-            self.printBoard()
-            print(f'\nWINNER: {self.board[1]}')
-            self.current_turn = self.board[1]
-            return False
-        elif self.board[2] == self.board[5] == self.board[8] != ' ':
-            self.printBoard()
-            print(f'\nWINNER: {self.board[2]}')
-            self.current_turn = self.board[2]
-            return False
-        elif ' ' not in self.board:
+        
+        win_check = [self.board[0:3], self.board[3:6], self.board[:6],self.board[0:7:3], \
+            self.board[1:8:3], self.board[1::3], self.board[2::3],  self.board[0::4], self.board[2:7:2]]
+
+        for i in win_check:
+            if all(x == i[0] for x in i)  and i[0] != ' ':
+                self.printBoard()
+                print(f'\nWINNER: {i[0]}')
+                self.current_turn = i[0]
+                return False
+        
+        if ' ' not in self.board:
             self.printBoard()
             print('\nDRAW\n')
             return False
         else: return True
     
-    #player move
+    #Let player select a tile.
     def playerMove(self):
         self.printBoard()
         print()
@@ -102,23 +73,20 @@ class Game(object):
 
         self.current_turn = 'X'
     
-    #bot move
+    #Minimax ai
     def botMove(self):
+        
+        def gameOverBot(): 
+            win_check = [self.board[0:3], self.board[3:6], self.board[:6],self.board[0:7:3], \
+            self.board[1:8:3], self.board[1::3], self.board[2::3],  self.board[0::4], self.board[2:7:2]]
 
-        def gameOverBot():
-            if self.board[0] == self.board[1] == self.board[2] != ' ': return self.board[0]
-            elif self.board[3] == self.board[4] == self.board[5] != ' ': return self.board[3]
-            elif self.board[6] == self.board[7] == self.board[8] != ' ': return self.board[6]
-            elif self.board[2] == self.board[4] == self.board[6] != ' ': return self.board[2]
-            elif self.board[0] == self.board[4] == self.board[8] != ' ': return self.board[0]
-            elif self.board[0] == self.board[3] == self.board[6] != ' ': return self.board[0]
-            elif self.board[1] == self.board[4] == self.board[7] != ' ': return self.board[1]
-            elif self.board[2] == self.board[5] == self.board[8] != ' ': return self.board[2]
-            elif ' ' not in self.board: return '--'
+            for i in win_check:
+                if all(x == i[0] for x in i)  and i[0] != ' ': return i[0]
+            
+            if ' ' not in self.board: return '--'
             else: return None
         
-        def max(a, b):
-            
+        def max(a, b):    
             maxi = -2
             move = None
             results = gameOverBot()
@@ -143,8 +111,7 @@ class Game(object):
 
             return [maxi, move]
             
-        def min(a, b):
-            
+        def min(a, b):   
             mini = 2
             move = None
             results = gameOverBot()
