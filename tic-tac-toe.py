771dc7bb11d544eab216b57cf00ef6fead8c81fc
justin-wittenmeier <71291781+justin-wittenmeier@users.noreply.github.com>
2020-12-23 14:19:58 -0600

    Update tic-tac-toe.py
    Updated readability.

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,56 +1,53 @@
 #Tic Tak Toe Update
 class Game(object):
     def __init__(self):
-        #Empty board for game
+        #Generate board
         self.board = [' '] * 9
-        
-        #Sets current turn to player by default.
         self.current_turn = 'O'
-
-        #On/off switch to restart the game
         self.game_on = False
 
-    #Clear the board for a new game.
+    #Wipe board
     def clearBoard(self):
         self.board = [' '] * 9
     
-    #Print the board for the current match
+    #Print board
     def printBoard(self): print(f'Current Board:\n{self.board[6]}|{self.board[7]}|{self.board[8]}\n-+-+-\n{self.board[3]}|{self.board[4]}|{self.board[5]}\n-+-+-\n{self.board[0]}|{self.board[1]}|{self.board[2]}')
     
-    #Let the player know how to make moves in the game.
+    #Print board info
     def printInfoBoard(self): print('Select Move:\n7|8|9\n-+-+-\n4|5|6\n-+-+-\n1|2|3')
 
-    #Idle Start Screen
+    #Prompt user to start the game
     def startGame(self):
         input('Enter anything to start game: ')
         self.clearBoard()
         return True
 
-    #Cycle player/bot moves.
+    #Check whos turn it is
     def runGame(self):
         if self.current_turn == 'O': self.playerMove()
         else: self.botMove()
 
-    #Check if the game is still in progress or not and gets the winner.
+    #Check if the macth is over
     def gameOver(self):
-        
+        #List of all possible wins
         win_check = [self.board[0:3], self.board[3:6], self.board[:6],self.board[0:7:3], \
             self.board[1:8:3], self.board[1::3], self.board[2::3],  self.board[0::4], self.board[2:7:2]]
 
+        #Check for a win
         for i in win_check:
             if all(x == i[0] for x in i)  and i[0] != ' ':
                 self.printBoard()
                 print(f'\nWINNER: {i[0]}')
                 self.current_turn = i[0]
                 return False
-        
+        #Check for a draw
         if ' ' not in self.board:
             self.printBoard()
             print('\nDRAW\n')
             return False
         else: return True
     
-    #Let player select a tile.
+    #Prompt player to perform move and executes desired move.
     def playerMove(self):
         self.printBoard()
         print()
@@ -73,20 +70,20 @@ class Game(object):
 
         self.current_turn = 'X'
     
-    #Minimax ai
+    #The bots moves are decided via a minimax algorithm with alpha beta filtering
     def botMove(self):
-        
-        def gameOverBot(): 
+
+        def gameOverBot():
+            #List of all possible wins
             win_check = [self.board[0:3], self.board[3:6], self.board[:6],self.board[0:7:3], \
             self.board[1:8:3], self.board[1::3], self.board[2::3],  self.board[0::4], self.board[2:7:2]]
 
             for i in win_check:
-                if all(x == i[0] for x in i)  and i[0] != ' ': return i[0]
-            
+                if all(x == i[0] for x in i) and i[0] != ' ': return i[0]
             if ' ' not in self.board: return '--'
             else: return None
         
-        def max(a, b):    
+        def max(a, b):
             maxi = -2
             move = None
             results = gameOverBot()
@@ -103,15 +100,18 @@ class Game(object):
                     if m[0] > maxi:
                         maxi = m[0]
                         move = i
-                    
+
                     self.board[i] = ' '
 
-                    if maxi >= b: return [maxi, move]
-                    if maxi > a: a = maxi
-
+                    if maxi >= b:
+                        return [maxi, move]
+                    if maxi > a:
+                        a = maxi
+                                
             return [maxi, move]
             
-        def min(a, b):   
+        def min(a, b):
+            
             mini = 2
             move = None
             results = gameOverBot()
@@ -131,19 +131,18 @@ class Game(object):
                     
                     self.board[i] = ' '
 
-                    if mini <= a: return [mini, move]
-                    if mini < b: b = mini
+                    if mini <= a:
+                        return [mini, move]
+                    if mini < b:
+                        b = mini
             
             return [mini, move]
         
         r = max(-2, 2)
-        
-
         self.board[r[1]] = 'X'
-        
         self.current_turn = 'O'
 
-#runs the game on a loop
+#Loop the game to allow rematches.
 game = Game()
 
 while True:
