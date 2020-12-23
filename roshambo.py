652a122dd57715b1cc33fded4263f250f80d27f2
justin-wittenmeier <71291781+justin-wittenmeier@users.noreply.github.com>
2020-12-23 15:04:18 -0600

    Update roshambo.py

--- roshambo.py
+++ roshambo.py
@@ -1,45 +1,54 @@
 import random
 
-def roshambo():
-    # valid moves
-    moves = ['rock','paper','scissors']
-    #bot move
-    bot_choice = lambda :random.choice(moves)
-    #player move
-    def player_choice():
-        alt_letter_inputs = ['r', 'p', 's']
+#Roshambo update.
+class Game:
+    def __init__(self):
+        #List of moves.
+        self.moves = ['rock','paper','scissors']
+        #Selects a move at random for the bot. 
+        self.botChoice = lambda: random.choice(self.moves)
+    
+    #Gets the players move.
+    def playerChoice(self):
+        #Get the first letter from each input to allow for easier play.
+        alt_inputs = [i[0] for i in self.moves]
+        #Get move from player.
         move = input('1: Rock\n2: Paper\n3: Scissors\nSelect Move: ').lower()
-
-        if move not in moves and move not in alt_letter_inputs:
-            try: 
-                move = int(move) - 1
+        #Catch wrong inputs.
+        if move not in self.moves and move not in alt_inputs:
+            try:
+                move = int(move) -1
             except ValueError:
                 print('\nERROR: Please select a valid move.\n')
-                return player_choice()
+                return self.playerChoice()
             if 0 < move > 2:
                 print('\nERROR: Please select a valid move.\n')
-                return player_choice()
-            move = moves[move]
-        
-        if move == 'r': move = moves[0]
-        if move == 'p': move = moves[1]
-        if move == 's': move = moves[2]
-
+                return self.playerChoice()
+            move = self.moves[move]
+        #Alt Input Convert
+        if move == 'r': move = self.moves[0]
+        if move == 'p': move = self.moves[1]
+        if move == 's': move = self.moves[2]
         return move
-    #checks winner and dislplays results
-    def win_check(p, b):
-        if p == b: print('\nResults: Draw\n')
-        elif p == moves[1] and b == moves[0]: print('\nResults: Player Wins!\n')
-        elif p == moves[2] and b == moves[1]: print('\nResults: Player Wins!\n')
-        elif p == moves[0] and b == moves[2]: print('\nResults: Player Wins!\n')
-        elif b == moves[1] and p == moves[0]: print('\nResults: Computer Wins!\n')
-        elif b == moves[2] and p == moves[1]: print('\nResults: Computer Wins!\n')
-        elif b == moves[0] and p == moves[2]: print('\nResults: Computer Wins!\n')
-        else: print(f'\n{b}\n')
-
-
-    print('')
-
-    win_check(player_choice(), bot_choice())
-
-roshambo()
+        
+    #Check the winner and display results.
+    def winCheck(self, player, bot):
+        win_form = ['\nResults: Draw\n', '\nResults: Player Wins!\n', '\nResults: Computer Wins!\n']
+        if player == bot: print(win_form[0])
+        elif player == self.moves[0]:
+            if bot == self.moves[2]: print(win_form[1])
+            else: print(win_form[2])
+        elif player == self.moves[1]:
+            if bot == self.moves[0]: print(win_form[1])
+            else: print(win_form[2])
+        elif player == self.moves[2]:
+            if bot == self.moves[1]: print(win_form[1])
+            else: print(win_form[2])
+    
+    def main(self):
+        self.winCheck(self.playerChoice(), self.botChoice())
+                
+#Run game.
+if __name__ == "__main__":
+    game = Game()
+    game.main()
